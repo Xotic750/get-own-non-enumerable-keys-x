e163c71fdff8f7983a2b3dbd1e78effa5a58972f
Graham Fairweather <xotic750@gmail.com>
2019-07-16 21:52:52 +0200

    Terraformed
    Auto build

--- src/get-own-non-enumerable-keys-x.js
+++ src/get-own-non-enumerable-keys-x.js
@@ -1,12 +1,3 @@
-/**
- * @file Like Reflect.ownKeys but gets only non-enumerable properties.
- * @version 3.2.0
- * @author Xotic750 <Xotic750@gmail.com>
- * @copyright  Xotic750
- * @license {@link <https://opensource.org/licenses/MIT> MIT}
- * @module get-own-non-enumerable-keys-x
- */
-
 import toObject from 'to-object-x';
 
 import filter from 'array-filter-x';
@@ -23,20 +14,6 @@ import getOEPS from 'get-own-enumerable-property-symbols-x';
  * @param {object} target - The target.
  * @throws {typeError} - If target is null or undefined.
  * @returns {Array} The non-enumerable own keys.
- * @example
- * var getOwnNonEnumerableKeys = require('get-own-non-enumerable-keys-x');
- *
- * var obj = { bar: 1, foo: 2 };
- * Object.defineProperty(obj, '1', {
- *   value: 'first'
- * });
- *
- * var symbol = Symbol('first');
- * Object.defineProperty(obj, symbol, {
- *   value: 'second'
- * });
- *
- * getOwnNonEnumerableKeys(obj); // ['1', symbol]
  */
 export default function getOwnNonEnumerableKeys(target) {
   const object = toObject(target);
